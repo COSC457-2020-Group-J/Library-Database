5b9d521e3ed241641fc644d911396d6116ef937a
Matthew Kline <mik228@live.com>
2020-11-24 07:22:28 -0500

    Insertion script, program successfully queries database

---
+++ libraryDatabase/sql/insertion.sql
@@ -0,0 +1,33 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+INSERT INTO BOOK(Book_name, Author_number, Pub_number, Date_published)
+VALUES('My First Book', 1, 1, '2020-01-01');
+
+INSERT INTO PUBLISHER(Pub_name, City, Country, Telephone)
+VALUES('Book House', 'Baltimore', 'United States', 4105551212);
+
+INSERT INTO AUTHOR(Person_number)
+VALUES(1);
+
+INSERT INTO BORROWER(Person_number, Book_number, Date_borrowed, Date_due)
+VALUES(1, 1, '2020-2-15', '2020-3-15');
+
+INSERT INTO PERSON(First_name, Last_name, City, Country)
+VALUES('John', 'Smith', 'Towson', 'United States');
+
+INSERT INTO BRANCH(City, Country, Telephone)
+VALUES('Ocean City', 'United States', 4435551212);
+
+INSERT INTO LIBRARIAN(Person_number, Branch_number, Wage)
+VALUES(1, 1, 15);
+
+INSERT INTO JOURNAL(Author_number, News_number, Journal_name)
+VALUES(1, 1, 'My First Journal');
+
+INSERT INTO ARTICLE(Author_number, News_number, Article_name)
+VALUES(1, 1, 'My First Article');
+
+INSERT INTO NEWSLETTER(Date_published)
+VALUES('2020-4-20');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- libraryDatabase/src/libraryDatabase/Main.java
+++ libraryDatabase/src/libraryDatabase/Main.java
@@ -3,8 +3,8 @@ package libraryDatabase;
 import java.sql.*;
 
 public class Main {
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://localhost:3306";
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/sys";
 
 	static final String USER = "root";
 	static final String PASS = "password";
@@ -12,15 +12,30 @@ public class Main {
 	public static void main(String[] args) {
 		Connection conn = null;
 		Statement stmt = null;
-		
+
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
 			stmt = conn.createStatement();
 			String sql;
-			sql = "SELECT * FROM *";
+			sql = "SELECT Book_number, Book_name, Author_number, Pub_number, Date_published FROM BOOK";
 			ResultSet rs = stmt.executeQuery(sql);
-			
+
+			while(rs.next()) {
+				int Book_number = rs.getInt("Book_number");
+				String Book_name = rs.getString("Book_name");
+				int Author_number = rs.getInt("Author_number");
+				int Pub_number = rs.getInt("Pub_number");
+				String Date_published = rs.getString("Date_published");
+
+				System.out.print("Book number: " + Book_number);
+				System.out.print(", Book name: " + Book_name);
+				System.out.print(", Author number: " + Author_number);
+				System.out.print(", Publisher number: " + Pub_number);
+				System.out.println(", Date published: " + Date_published);
+			}
+
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -28,7 +43,17 @@ public class Main {
 			se.printStackTrace();
 		} catch(Exception e) {
 			e.printStackTrace();
-		} finally {
+		}finally{
+			try{
+				if(stmt!=null)
+					stmt.close();
+			} catch(SQLException se2){
+			} try{
+				if(conn!=null)
+					conn.close();
+			} catch(SQLException se){
+				se.printStackTrace();
+			}
 		}
 	}
 }
