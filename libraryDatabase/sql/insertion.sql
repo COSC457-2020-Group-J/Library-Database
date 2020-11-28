3b8b904e894c6809914e09bb2d8a5dc9ce76bae7
Matthew Kline <mik228@live.com>
2020-11-27 19:18:42 -0500

    Added to insertion script

--- libraryDatabase/sql/insertion.sql
+++ libraryDatabase/sql/insertion.sql
@@ -2,33 +2,79 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 INSERT INTO BOOK(Book_name, Author_number, Pub_number, Date_published)
 VALUES('My First Book', 1, 1, '2020-01-01');
+INSERT INTO BOOK(Book_name, Author_number, Pub_number, Date_published)
+VALUES('The Second Book', 7, 1, '2020-02-15');
+INSERT INTO BOOK(Book_name, Author_number, Pub_number, Date_published)
+VALUES('Book the Third', 8, 1, '2020-03-03');
+INSERT INTO BOOK(Book_name, Author_number, Pub_number, Date_published)
+VALUES('4th Book', 9, 1, '2020-01-01');
 
 INSERT INTO PUBLISHER(Pub_name, City, Country, Telephone)
 VALUES('Book House', 'Baltimore', 'United States', 4105551212);
+INSERT INTO PUBLISHER(Pub_name, City, Country, Telephone)
+VALUES('Towson Books', 'Towson', 'United States', 4105551234);
+INSERT INTO PUBLISHER(Pub_name, City, Country, Telephone)
+VALUES('Publishers', 'Annapolis', 'United States', 4105559876);
 
 INSERT INTO AUTHOR(Person_number)
 VALUES(1);
+INSERT INTO AUTHOR(Person_number)
+VALUES(7);
+INSERT INTO AUTHOR(Person_number)
+VALUES(8);
+INSERT INTO AUTHOR(Person_number)
+VALUES(9);
 
 INSERT INTO BORROWER(Person_number, Book_number, Date_borrowed, Date_due)
 VALUES(1, 1, '2020-2-15', '2020-3-15');
+INSERT INTO BORROWER(Person_number, Book_number, Date_borrowed, Date_due)
+VALUES(10, 1, '2020-3-06', '2020-4-06');
+INSERT INTO BORROWER(Person_number, Book_number, Date_borrowed, Date_due)
+VALUES(9, 1, '2020-5-01', '2020-6-01');
 
 INSERT INTO PERSON(First_name, Last_name, City, Country)
 VALUES('John', 'Smith', 'Towson', 'United States');
+INSERT INTO PERSON(First_name, Last_name, City, Country)
+VALUES('Dan', 'Daniels', 'Baltimore', 'United States');
+INSERT INTO PERSON(First_name, Last_name, City, Country)
+VALUES('Mike', 'Michaels', 'Towson', 'United States');
+INSERT INTO PERSON(First_name, Last_name, City, Country)
+VALUES('Mark', 'Smith', 'Towson', 'United States');
 
 INSERT INTO BRANCH(City, Country, Telephone)
 VALUES('Ocean City', 'United States', 4435551212);
+INSERT INTO BRANCH(City, Country, Telephone)
+VALUES('Towson', 'United States', 4435554321);
+INSERT INTO BRANCH(City, Country, Telephone)
+VALUES('Baltimore', 'United States', 4435551928);
 
 INSERT INTO LIBRARIAN(Person_number, Branch_number, Wage)
 VALUES(1, 1, 15);
+INSERT INTO LIBRARIAN(Person_number, Branch_number, Wage)
+VALUES(7, 2, 20);
+INSERT INTO LIBRARIAN(Person_number, Branch_number, Wage)
+VALUES(8, 3, 25);
 
 INSERT INTO JOURNAL(Author_number, News_number, Journal_name)
 VALUES(1, 1, 'My First Journal');
+INSERT INTO JOURNAL(Author_number, News_number, Journal_name)
+VALUES(7, 2, 'Journalism');
 
 INSERT INTO ARTICLE(Author_number, News_number, Article_name)
 VALUES(1, 1, 'My First Article');
+INSERT INTO ARTICLE(Author_number, News_number, Article_name)
+VALUES(8, 3, 'New Books Available');
 
 INSERT INTO NEWSLETTER(Date_published)
 VALUES('2020-4-20');
+INSERT INTO NEWSLETTER(Date_published)
+VALUES('2020-5-20');
+INSERT INTO NEWSLETTER(Date_published)
+VALUES('2020-6-20');
+INSERT INTO NEWSLETTER(Date_published)
+VALUES('2020-7-20');
+INSERT INTO NEWSLETTER(Date_published)
+VALUES('2020-8-20');
 
 INSERT INTO USER(Username, Password)
 VALUES('admin', 'password');
